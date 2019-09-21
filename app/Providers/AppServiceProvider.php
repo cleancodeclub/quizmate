12201e24e1e0a33ba81ebbb9953193b97eed1481
Kevin Jamaul Chisholm <kevinjchisholm@gmail.com>
2019-09-21 17:58:45 +0900

    removed unsafe script warning

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,11 +24,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Force SSL in production
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
     }
-}
-
-// Force SSL in production
-if ($this->app->environment() == 'production') {
-    URL::forceScheme('https');
 }
