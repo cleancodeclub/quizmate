4a56458a259718a7a6382d488561efff8e870104
Kevin Jamaul Chisholm <kevinjchisholm@gmail.com>
2019-10-05 12:01:10 +0900

    added frontend base query

---
+++ resources/js/components/Shuffler.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    Api Shuffler
+    <div>
+      Data:
+      <div v-for="category in categories" :key="category.id">{{ category }}</div>
+    </div>
+    <button @click="getAllCatagories">Get Categories</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "shuffler",
+
+  data() {
+    return {
+      categories: []
+    };
+  },
+
+  methods: {
+    async getAllCatagories() {
+      const res = await axios.post(
+        "https://quiz-mate-api.herokuapp.com/graphql",
+        {
+          // const res = await axios.post("http://quiz-mate-server.test/graphql", {
+          query: `{
+            allCategories {
+              name
+            }
+          }`
+        }
+      );
+      this.categories = res.data.data;
+      console.log(res.data);
+    }
+  }
+};
+</script>
